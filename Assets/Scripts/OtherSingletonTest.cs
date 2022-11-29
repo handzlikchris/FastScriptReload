b5f8a61cb7431afad6e6211e6167ad8c1a45a800
Chris Handzlik <chris@reliable-solutions.co.uk>
2022-11-29 15:15:39 +0000

    added references to working Roslyn libraries to parse source code

--- Assets/Scripts/OtherSingletonTest.cs
+++ Assets/Scripts/OtherSingletonTest.cs
@@ -1,5 +1,9 @@
-using System;
 using System.Collections;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using UnityEngine;
 
 namespace SomeNamespace
@@ -26,6 +30,26 @@ namespace SomeNamespace
         void PrintExistingSingletonValue()
         {
             Debug.Log($"PrintExistingSingletonValue-c: {ExistingSingletonTest.Instance._intValue}"); 
+        }
+
+        [ContextMenu(nameof(Test))] 
+        void Test()
+        {
+            var content = File.ReadAllText(@"E:\_src-unity\QuickCodeIteration\Assets\QuickCodeIteration\Examples\Scripts\FunctionLibrary.cs");   
+            var tree = CSharpSyntaxTree.ParseText(content);
+	
+// tree.DumpSyntaxTree();
+            var root = tree.GetRoot();
+            var klass = root.DescendantNodes().OfType<ClassDeclarationSyntax>().First();
+            var existingIdentifier = klass.ChildTokens().Where(k => k.RawKind == (int)SyntaxKind.IdentifierToken).First();
+
+            var newIdentifier = SyntaxFactory.Identifier(existingIdentifier.Value + "__Patched_");
+            var updateClass = klass.ReplaceToken(existingIdentifier, newIdentifier);
+	
+            var updatedRoot = root.ReplaceNode(klass, updateClass);
+	
+            var result = updatedRoot.ToFullString();
+            Debug.Log(result);
         }
 
         private void Start()
