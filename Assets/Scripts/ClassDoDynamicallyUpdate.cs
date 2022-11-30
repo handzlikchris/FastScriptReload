b985efce6c376f861d6f25c8534e9df6633f267e
Chris Handzlik <chris@reliable-solutions.co.uk>
2022-11-30 11:47:58 +0000

    added more logging for detour creation (if compiled with QuickCodeIterationManager_DebugEnabled)

--- Assets/Scripts/ClassDoDynamicallyUpdate.cs
+++ Assets/Scripts/ClassDoDynamicallyUpdate.cs
@@ -1,9 +1,27 @@
 ﻿using UnityEngine;
 
-public class ClassDoDynamicallyUpdate: MonoBehaviour
+public class ClassDoDynamicallyUpdate: BaseClass
 {
     void Update()
     {
         Debug.Log("Testing - 2");
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(new Vector3(0, 6, 0), Vector3.one);
+    }
+    
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawCube(new Vector3(0, 2, 0), Vector3.one);
+    }
+}
+
+public class BaseClass : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(new Vector3(0, 3, 0), Vector3.one);
     }
 }
