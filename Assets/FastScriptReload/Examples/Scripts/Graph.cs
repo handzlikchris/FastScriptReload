2223dd8f96b014dac7e46b75e51512ecedaac3e5
Chris Handzlik <chris@reliable-solutions.co.uk>
2022-12-04 13:53:39 +0000

    example fix

--- Assets/FastScriptReload/Examples/Scripts/Graph.cs
+++ Assets/FastScriptReload/Examples/Scripts/Graph.cs
@@ -47,7 +47,7 @@ namespace FastScriptReload.Examples
 			var time = Time.time;
 			var step = 2f / resolution;
 			var v = 0.5f * step - 1f;
-			for (int i = 0, x = 0, z = 0; i < points.Length / 2; i++, x++)
+			for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
 			{
 				if (x == resolution)
 				{
