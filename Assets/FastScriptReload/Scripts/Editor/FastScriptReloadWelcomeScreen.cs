9f7f942b5db3eff067f27acc9558db463dc7a7e1
Chris Handzlik <chris@reliable-solutions.co.uk>
2022-12-28 11:40:13 +0000

    upversioned to 1.1

--- Assets/FastScriptReload/Scripts/Editor/FastScriptReloadWelcomeScreen.cs
+++ Assets/FastScriptReload/Scripts/Editor/FastScriptReloadWelcomeScreen.cs
@@ -21,7 +21,7 @@ namespace FastScriptReload.Editor
         {
             return $"{BaseUrl}/updates/fast-script-reload/{userId}?CurrentVersion={versionId}";
         }
-        public static string VersionId = "1.0";
+        public static string VersionId = "1.1";
         private static readonly string ProjectIconName = "ProductIcon64";
         public static readonly string ProjectName = "fast-script-reload";
 
