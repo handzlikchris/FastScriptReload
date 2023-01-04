c641e4ca6340c0599b012c5ef012a98fae8df0ca
Chris Handzlik <chris@reliable-solutions.co.uk>
2023-01-04 12:32:21 +0000

    create common Logger class and Logdebug method

---
+++ Assets/FastScriptReload/Scripts/Runtime/Logger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace FastScriptReload.Runtime
+{
+    //TODO: move to common and just set LogPrefix as needed
+    public static class Logger
+    {
+        public static string LogPrefix = "FSR: ";
+
+        public static void LogDebug(string message)
+        {
+#if FastScriptReload_DebugEnabled
+            Debug.Log($"<color=#ABABAB>{LogPrefix}{message}</color>");    
+#endif
+        }
+
+        public static void Log(string message) => Debug.Log(LogPrefix + message);
+    }
+}
