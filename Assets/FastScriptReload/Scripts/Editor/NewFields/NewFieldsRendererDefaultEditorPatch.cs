944b40315545d034cc9067bb6558d0888ea4dcbe
Chris Handzlik <chris@reliable-solutions.co.uk>
2023-01-24 09:49:20 +0000

    reformatted

--- Assets/FastScriptReload/Scripts/Editor/NewFields/NewFieldsRendererDefaultEditorPatch.cs
+++ Assets/FastScriptReload/Scripts/Editor/NewFields/NewFieldsRendererDefaultEditorPatch.cs
@@ -69,64 +69,30 @@ namespace FastScriptReload.Editor.NewFields
                             var existingValueType = (Type)newFieldNameToGetTypeFn[addedFieldValueKey]();
 
                             //rendering types come from UnityEditor.EditorGUI.DefaultPropertyField - that should handle all cases that editor can render
-                            if (existingValueType == typeof(int))
-                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.IntField(addedFieldValueKey,
-                                    (int)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(bool))
-                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.Toggle(addedFieldValueKey,
-                                    (bool)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(float))
-                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.FloatField(addedFieldValueKey,
-                                    (float)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(string))
-                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.TextField(addedFieldValueKey,
-                                    (string)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(Color))
-                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.ColorField(addedFieldValueKey,
-                                    (Color)addedFieldValues[addedFieldValueKey]);
+                            if (existingValueType == typeof(int)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.IntField(addedFieldValueKey, (int)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(bool)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.Toggle(addedFieldValueKey, (bool)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(float)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.FloatField(addedFieldValueKey, (float)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(string)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.TextField(addedFieldValueKey, (string)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(Color)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.ColorField(addedFieldValueKey, (Color)addedFieldValues[addedFieldValueKey]);
                             //TODO: how to handle SerializedPropertyType.ObjectReference? //initially disallow?
                             // else if (existingValueType == typeof(object)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.Toggle(addedFieldValueKey, (bool)addedFieldValues[addedFieldValueKey]);
                             //TODO: SerializedPropertyType.LayerMask
-                            else if (existingValueType == typeof(Enum))
-                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.EnumPopup(addedFieldValueKey,
-                                    (Enum)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(Vector2))
-                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.Vector2Field(addedFieldValueKey,
-                                    (Vector2)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(Vector3))
-                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.Vector3Field(addedFieldValueKey,
-                                    (Vector3)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(Vector4))
-                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.Vector4Field(addedFieldValueKey,
-                                    (Vector4)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(Rect))
-                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.RectField(addedFieldValueKey,
-                                    (Rect)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(Enum)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.EnumPopup(addedFieldValueKey, (Enum)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(Vector2)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.Vector2Field(addedFieldValueKey, (Vector2)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(Vector3)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.Vector3Field(addedFieldValueKey, (Vector3)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(Vector4)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.Vector4Field(addedFieldValueKey, (Vector4)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(Rect)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.RectField(addedFieldValueKey, (Rect)addedFieldValues[addedFieldValueKey]);
                             //TODO: SerializedPropertyType.ArraySize
                             //TODO: SerializedPropertyType.Character
                             // else if (existingValueType == typeof(char)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.TextField(addedFieldValueKey, (char)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(AnimationCurve))
-                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.CurveField(addedFieldValueKey,
-                                    (AnimationCurve)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(Bounds))
-                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.BoundsField(addedFieldValueKey,
-                                    (Bounds)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(Gradient))
-                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.GradientField(addedFieldValueKey,
-                                    (Gradient)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(AnimationCurve)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.CurveField(addedFieldValueKey, (AnimationCurve)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(Bounds)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.BoundsField(addedFieldValueKey, (Bounds)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(Gradient)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.GradientField(addedFieldValueKey, (Gradient)addedFieldValues[addedFieldValueKey]);
                             //TODO: SerializedPropertyType.FixedBufferSize
-                            else if (existingValueType == typeof(Vector2Int))
-                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.Vector2IntField(addedFieldValueKey,
-                                    (Vector2Int)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(Vector3Int))
-                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.Vector3IntField(addedFieldValueKey,
-                                    (Vector3Int)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(RectInt))
-                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.RectIntField(addedFieldValueKey,
-                                    (RectInt)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(BoundsInt))
-                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.BoundsIntField(addedFieldValueKey,
-                                    (BoundsInt)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(Vector2Int)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.Vector2IntField(addedFieldValueKey, (Vector2Int)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(Vector3Int)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.Vector3IntField(addedFieldValueKey, (Vector3Int)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(RectInt)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.RectIntField(addedFieldValueKey, (RectInt)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(BoundsInt)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.BoundsIntField(addedFieldValueKey, (BoundsInt)addedFieldValues[addedFieldValueKey]);
                             //TODO: SerializedPropertyType.Hash128
                             else if (existingValueType == typeof(Quaternion))
                             {
