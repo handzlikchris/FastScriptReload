89337fdafd1db2b61288d2f84691ace95e710649
Chris Handzlik <chris@reliable-solutions.co.uk>
2023-01-24 09:27:56 +0000

    custom editors will also render additional fields

--- Assets/FastScriptReload/Scripts/Editor/NewFields/NewFieldsRendererDefaultEditorPatch.cs
+++ Assets/FastScriptReload/Scripts/Editor/NewFields/NewFieldsRendererDefaultEditorPatch.cs
@@ -19,15 +19,31 @@ namespace FastScriptReload.Editor.NewFields
         
         static NewFieldsRendererDefaultEditorPatch()
         {
-            var harmony = new Harmony(nameof(NewFieldsRendererDefaultEditorPatch));
+            if ((bool)FastScriptReloadPreference.EnableExperimentalAddedFieldsSupport.GetEditorPersistedValueOrDefault())
+            {
+                var harmony = new Harmony(nameof(NewFieldsRendererDefaultEditorPatch));
             
-            var original =  AccessTools.Method("UnityEditor.GenericInspector:OnOptimizedInspectorGUI");
-            var prefix = AccessTools.Method(typeof(NewFieldsRendererDefaultEditorPatch), nameof(OnOptimizedInspectorGUI));
+                var renderAdditionalFieldsOnOptimizedGuiPostfix = AccessTools.Method(typeof(NewFieldsRendererDefaultEditorPatch), nameof(OnOptimizedInspectorGUI));
+                var noCustomEditorOriginalRenderingMethdod =  AccessTools.Method("UnityEditor.GenericInspector:OnOptimizedInspectorGUI");
+                harmony.Patch(noCustomEditorOriginalRenderingMethdod, postfix: new HarmonyMethod(renderAdditionalFieldsOnOptimizedGuiPostfix));
             
-            harmony.Patch(original, postfix: new HarmonyMethod(prefix));
+                var renderAdditionalFieldsDrawDefaultInspectorPostfix = AccessTools.Method(typeof(NewFieldsRendererDefaultEditorPatch), nameof(DrawDefaultInspector));
+                var customEditorRenderingMethod = AccessTools.Method("UnityEditor.Editor:DrawDefaultInspector");
+                harmony.Patch(customEditorRenderingMethod, postfix: new HarmonyMethod(renderAdditionalFieldsDrawDefaultInspectorPostfix)); 
+            }
         }
 
         private static void OnOptimizedInspectorGUI(Rect contentRect, UnityEditor.Editor __instance)
+        {
+            RenderNewlyAddedFields(__instance);
+        }
+        
+        private static void DrawDefaultInspector(UnityEditor.Editor __instance)
+        {
+            RenderNewlyAddedFields(__instance);
+        }
+
+        private static void RenderNewlyAddedFields(UnityEditor.Editor __instance)
         {
             //TODO: perf optimize, this will be used for many types, perhaps keep which types changed and just pass type?
             if (__instance.target)
@@ -35,17 +51,18 @@ namespace FastScriptReload.Editor.NewFields
                 if (TemporaryNewFieldValues.TryGetDynamicallyAddedFieldValues(__instance.target, out var addedFieldValues))
                 {
                     EditorGUILayout.Space(10);
-                    
-                    EditorGUILayout.BeginHorizontal(); 
+
+                    EditorGUILayout.BeginHorizontal();
                     EditorGUILayout.LabelField("[FSR] Dynamically Added Fields:");
-                    GuiTooltipHelper.AddHelperTooltip("Fields were dynamically added for hot-reload, you can adjust their values and on full reload they'll disappear from this section and move back to main one.");
+                    GuiTooltipHelper.AddHelperTooltip(
+                        "Fields were dynamically added for hot-reload, you can adjust their values and on full reload they'll disappear from this section and move back to main one.");
                     EditorGUILayout.EndHorizontal();
-                    EditorGUILayout.Space(5); 
+                    EditorGUILayout.Space(5);
 
                     try
                     {
                         _cachedKeys.AddRange(addedFieldValues.Keys);
-                        
+
                         var newFieldNameToGetTypeFn = CreateNewFieldInitMethodRewriter.ResolveNewFieldsToTypeFn(
                             AssemblyChangesLoader.Instance.GetRedirectedType(__instance.target.GetType())
                         );
@@ -54,44 +71,80 @@ namespace FastScriptReload.Editor.NewFields
                             var existingValueType = (Type)newFieldNameToGetTypeFn[addedFieldValueKey]();
 
                             //rendering types come from UnityEditor.EditorGUI.DefaultPropertyField - that should handle all cases that editor can render
-                            if (existingValueType == typeof(int)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.IntField(addedFieldValueKey, (int)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(bool)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.Toggle(addedFieldValueKey, (bool)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(float)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.FloatField(addedFieldValueKey, (float)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(string)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.TextField(addedFieldValueKey, (string)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(Color)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.ColorField(addedFieldValueKey, (Color)addedFieldValues[addedFieldValueKey]);
+                            if (existingValueType == typeof(int))
+                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.IntField(addedFieldValueKey,
+                                    (int)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(bool))
+                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.Toggle(addedFieldValueKey,
+                                    (bool)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(float))
+                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.FloatField(addedFieldValueKey,
+                                    (float)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(string))
+                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.TextField(addedFieldValueKey,
+                                    (string)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(Color))
+                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.ColorField(addedFieldValueKey,
+                                    (Color)addedFieldValues[addedFieldValueKey]);
                             //TODO: how to handle SerializedPropertyType.ObjectReference? //initially disallow?
                             // else if (existingValueType == typeof(object)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.Toggle(addedFieldValueKey, (bool)addedFieldValues[addedFieldValueKey]);
                             //TODO: SerializedPropertyType.LayerMask
-                            else if (existingValueType == typeof(Enum)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.EnumPopup(addedFieldValueKey, (Enum)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(Vector2)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.Vector2Field(addedFieldValueKey, (Vector2)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(Vector3)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.Vector3Field(addedFieldValueKey, (Vector3)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(Vector4)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.Vector4Field(addedFieldValueKey, (Vector4)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(Rect)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.RectField(addedFieldValueKey, (Rect)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(Enum))
+                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.EnumPopup(addedFieldValueKey,
+                                    (Enum)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(Vector2))
+                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.Vector2Field(addedFieldValueKey,
+                                    (Vector2)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(Vector3))
+                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.Vector3Field(addedFieldValueKey,
+                                    (Vector3)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(Vector4))
+                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.Vector4Field(addedFieldValueKey,
+                                    (Vector4)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(Rect))
+                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.RectField(addedFieldValueKey,
+                                    (Rect)addedFieldValues[addedFieldValueKey]);
                             //TODO: SerializedPropertyType.ArraySize
                             //TODO: SerializedPropertyType.Character
                             // else if (existingValueType == typeof(char)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.TextField(addedFieldValueKey, (char)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(AnimationCurve)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.CurveField(addedFieldValueKey, (AnimationCurve)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(Bounds)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.BoundsField(addedFieldValueKey, (Bounds)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(Gradient)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.GradientField(addedFieldValueKey, (Gradient)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(AnimationCurve))
+                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.CurveField(addedFieldValueKey,
+                                    (AnimationCurve)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(Bounds))
+                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.BoundsField(addedFieldValueKey,
+                                    (Bounds)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(Gradient))
+                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.GradientField(addedFieldValueKey,
+                                    (Gradient)addedFieldValues[addedFieldValueKey]);
                             //TODO: SerializedPropertyType.FixedBufferSize
-                            else if (existingValueType == typeof(Vector2Int)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.Vector2IntField(addedFieldValueKey, (Vector2Int)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(Vector3Int)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.Vector3IntField(addedFieldValueKey, (Vector3Int)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(RectInt)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.RectIntField(addedFieldValueKey, (RectInt)addedFieldValues[addedFieldValueKey]);
-                            else if (existingValueType == typeof(BoundsInt)) addedFieldValues[addedFieldValueKey] = EditorGUILayout.BoundsIntField(addedFieldValueKey, (BoundsInt)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(Vector2Int))
+                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.Vector2IntField(addedFieldValueKey,
+                                    (Vector2Int)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(Vector3Int))
+                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.Vector3IntField(addedFieldValueKey,
+                                    (Vector3Int)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(RectInt))
+                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.RectIntField(addedFieldValueKey,
+                                    (RectInt)addedFieldValues[addedFieldValueKey]);
+                            else if (existingValueType == typeof(BoundsInt))
+                                addedFieldValues[addedFieldValueKey] = EditorGUILayout.BoundsIntField(addedFieldValueKey,
+                                    (BoundsInt)addedFieldValues[addedFieldValueKey]);
                             //TODO: SerializedPropertyType.Hash128
                             else if (existingValueType == typeof(Quaternion))
                             {
                                 //TODO: handled bit differently via EditorGUI.QuaternionEulerField(position, property, label);
                                 EditorGUILayout.BeginHorizontal();
                                 EditorGUILayout.LabelField($"{existingValueType.Name} - Unable to render");
-                                GuiTooltipHelper.AddHelperTooltip("Unable to handle added-field rendering for type: {existingValueType.Name}, it won't be rendered. Best workaround for now is to use Vector3 instead.");
+                                GuiTooltipHelper.AddHelperTooltip(
+                                    "Unable to handle added-field rendering for type: {existingValueType.Name}, it won't be rendered. Best workaround for now is to use Vector3 instead.");
                                 EditorGUILayout.EndHorizontal();
                             }
                             else
                             {
                                 EditorGUILayout.BeginHorizontal();
                                 EditorGUILayout.LabelField($"{existingValueType.Name} - Unable to render");
-                                GuiTooltipHelper.AddHelperTooltip("Unable to handle added-field rendering for type: {existingValueType.Name}, it won't be rendered. Best workaround is to not add this type dynamically in current version.");
+                                GuiTooltipHelper.AddHelperTooltip(
+                                    "Unable to handle added-field rendering for type: {existingValueType.Name}, it won't be rendered. Best workaround is to not add this type dynamically in current version.");
                                 EditorGUILayout.EndHorizontal();
                             }
                         }
