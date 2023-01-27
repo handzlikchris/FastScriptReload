c9912a0cf9657f5e97aab9a00b492b7f7850b157
Chris Handzlik <chris@reliable-solutions.co.uk>
2023-01-27 10:08:09 +0000

    refactor

--- Assets/FastScriptReload/Scripts/Editor/Compilation/CodeRewriting/NewFieldsRewriter.cs
+++ Assets/FastScriptReload/Scripts/Editor/Compilation/CodeRewriting/NewFieldsRewriter.cs
@@ -11,24 +11,48 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace FastScriptReload.Editor.Compilation.CodeRewriting
 {
-class NewFieldsRewriter : CSharpSyntaxRewriter
-{
-	private readonly Dictionary<string, List<string>> _typeToNewFieldDeclarations;
-
-	public NewFieldsRewriter(Dictionary<string, List<string>> typeToNewFieldDeclarations)
+	class NewFieldsRewriter : CSharpSyntaxRewriter
 	{
-		_typeToNewFieldDeclarations = typeToNewFieldDeclarations;
-	}
+		private readonly Dictionary<string, List<string>> _typeToNewFieldDeclarations;
 
-	public static List<MemberInfo> GetReplaceableMembers(Type type)
-	{ //TODO: later other might need to be included? props?
-		return type.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).Cast<MemberInfo>().ToList();
-	}
+		public NewFieldsRewriter(Dictionary<string, List<string>> typeToNewFieldDeclarations)
+		{
+			_typeToNewFieldDeclarations = typeToNewFieldDeclarations;
+		}
+
+		public static List<MemberInfo> GetReplaceableMembers(Type type)
+		{ //TODO: later other might need to be included? props?
+			return type.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).Cast<MemberInfo>().ToList();
+		}
 
 
-	public override SyntaxNode VisitInvocationExpression(InvocationExpressionSyntax node)
-	{
-		if (node.Expression.ToString() == "nameof")
+		public override SyntaxNode VisitInvocationExpression(InvocationExpressionSyntax node)
+		{
+			if (node.Expression.ToString() == "nameof")
+			{
+				var classNode = node.Ancestors().OfType<ClassDeclarationSyntax>().FirstOrDefault();
+				if (classNode != null)
+				{
+					var fullClassName = RoslynUtils.GetMemberFQDN(classNode, classNode.Identifier.ToString());
+					if (!string.IsNullOrEmpty(fullClassName))
+					{
+						var nameofExpressionParts = node.ArgumentList.Arguments.First().ToFullString().Split('.'); //nameof could have multiple . like NewFieldCustomClass.FieldInThatClass
+						var fieldName = nameofExpressionParts.First();  // should take first part only to determine if new field eg. 'NewFieldCustomClass'
+						if (_typeToNewFieldDeclarations.TryGetValue(fullClassName, out var allNewFieldNamesForClass))
+						{
+							if (allNewFieldNamesForClass.Contains(fieldName))
+							{
+								return SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression, SyntaxFactory.Literal(nameofExpressionParts.Last())); // should take last part only to for actual string eg. 'FieldInThatClass'
+							}
+						}
+					}
+				}
+			}
+
+			return base.VisitInvocationExpression(node);
+		}
+
+		public override SyntaxNode VisitIdentifierName(IdentifierNameSyntax node)
 		{
 			var classNode = node.Ancestors().OfType<ClassDeclarationSyntax>().FirstOrDefault();
 			if (classNode != null)
@@ -36,90 +60,66 @@ class NewFieldsRewriter : CSharpSyntaxRewriter
 				var fullClassName = RoslynUtils.GetMemberFQDN(classNode, classNode.Identifier.ToString());
 				if (!string.IsNullOrEmpty(fullClassName))
 				{
-					var nameofExpressionParts = node.ArgumentList.Arguments.First().ToFullString().Split('.'); //nameof could have multiple . like NewFieldCustomClass.FieldInThatClass
-					var fieldName = nameofExpressionParts.First();  // should take first part only to determine if new field eg. 'NewFieldCustomClass'
+					var fieldName = node.Identifier.ToString();
 					if (_typeToNewFieldDeclarations.TryGetValue(fullClassName, out var allNewFieldNamesForClass))
 					{
 						if (allNewFieldNamesForClass.Contains(fieldName))
 						{
-							return SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression, SyntaxFactory.Literal(nameofExpressionParts.Last())); // should take last part only to for actual string eg. 'FieldInThatClass'
+							var isNameOfExpression = node.Ancestors().OfType<InvocationExpressionSyntax>().Any(e => e.Expression.ToString() == "nameof");
+							if (!isNameOfExpression) //nameof expression will be rewritten via VisitInvocationExpression
+							{
+								return
+								SyntaxFactory.MemberAccessExpression(
+										SyntaxKind.SimpleMemberAccessExpression,
+										SyntaxFactory.InvocationExpression(
+												SyntaxFactory.MemberAccessExpression(
+													SyntaxKind.SimpleMemberAccessExpression,
+													SyntaxFactory.IdentifierName(typeof(TemporaryNewFieldValues).FullName),
+													SyntaxFactory.GenericName(
+															SyntaxFactory.Identifier(nameof(TemporaryNewFieldValues.ResolvePatchedObject)))
+														.WithTypeArgumentList(
+															SyntaxFactory.TypeArgumentList(
+																SyntaxFactory.SingletonSeparatedList<TypeSyntax>(
+																	SyntaxFactory.IdentifierName(fullClassName + AssemblyChangesLoader.ClassnamePatchedPostfix))))))
+											.WithArgumentList(
+												SyntaxFactory.ArgumentList(
+													SyntaxFactory.SingletonSeparatedList<ArgumentSyntax>(
+														SyntaxFactory.Argument(
+															SyntaxFactory.ThisExpression())))),
+										SyntaxFactory.IdentifierName(fieldName))
+									.WithTriviaFrom(node);
+							}
 						}
 					}
-				}
-			}
-		}
-
-		return base.VisitInvocationExpression(node);
-	}
-
-	public override SyntaxNode VisitIdentifierName(IdentifierNameSyntax node)
-	{
-		var classNode = node.Ancestors().OfType<ClassDeclarationSyntax>().FirstOrDefault();
-		if (classNode != null)
-		{
-			var fullClassName = RoslynUtils.GetMemberFQDN(classNode, classNode.Identifier.ToString());
-			if (!string.IsNullOrEmpty(fullClassName))
-			{
-				var fieldName = node.Identifier.ToString();
-				if (_typeToNewFieldDeclarations.TryGetValue(fullClassName, out var allNewFieldNamesForClass))
-				{
-					if (allNewFieldNamesForClass.Contains(fieldName))
+					else
 					{
-						var isNameOfExpression = node.Ancestors().OfType<InvocationExpressionSyntax>().Any(e => e.Expression.ToString() == "nameof");
-						if (!isNameOfExpression) //nameof expression will be rewritten via VisitInvocationExpression
-						{
-							return
-							SyntaxFactory.MemberAccessExpression(
-									SyntaxKind.SimpleMemberAccessExpression,
-									SyntaxFactory.InvocationExpression(
-											SyntaxFactory.MemberAccessExpression(
-												SyntaxKind.SimpleMemberAccessExpression,
-												SyntaxFactory.IdentifierName(typeof(TemporaryNewFieldValues).FullName),
-												SyntaxFactory.GenericName(
-														SyntaxFactory.Identifier(nameof(TemporaryNewFieldValues.ResolvePatchedObject)))
-													.WithTypeArgumentList(
-														SyntaxFactory.TypeArgumentList(
-															SyntaxFactory.SingletonSeparatedList<TypeSyntax>(
-																SyntaxFactory.IdentifierName(fullClassName + AssemblyChangesLoader.ClassnamePatchedPostfix))))))
-										.WithArgumentList(
-											SyntaxFactory.ArgumentList(
-												SyntaxFactory.SingletonSeparatedList<ArgumentSyntax>(
-													SyntaxFactory.Argument(
-														SyntaxFactory.ThisExpression())))),
-									SyntaxFactory.IdentifierName(fieldName))
-								.WithTriviaFrom(node);
-						}
+						LoggerScoped.LogWarning($"Unable to find type: {fullClassName}");
 					}
 				}
-				else
+			}
+
+			return base.VisitIdentifierName(node);
+		}
+
+		public override SyntaxNode VisitFieldDeclaration(FieldDeclarationSyntax node)
+		{
+			var fieldName = node.Declaration.Variables.First().Identifier.ToString();
+			var fullClassName = RoslynUtils.GetMemberFQDNWithoutMemberName(node);
+
+			if (_typeToNewFieldDeclarations.TryGetValue(fullClassName, out var newFields))
+			{
+				if (newFields.Contains(fieldName))
 				{
-					LoggerScoped.LogWarning($"Unable to find type: {fullClassName}");
+					var existingLeading = node.GetLeadingTrivia();
+					var existingTrailing = node.GetTrailingTrivia();
+
+					return node
+						.WithLeadingTrivia(existingLeading.Add(SyntaxFactory.Comment("/* ")))
+						.WithTrailingTrivia(existingTrailing.Insert(0, SyntaxFactory.Comment(" */ //Auto-excluded to prevent exceptions - see docs")));
 				}
 			}
+
+			return base.VisitFieldDeclaration(node);
 		}
-
-		return base.VisitIdentifierName(node);
 	}
-
-	public override SyntaxNode VisitFieldDeclaration(FieldDeclarationSyntax node)
-	{
-		var fieldName = node.Declaration.Variables.First().Identifier.ToString();
-		var fullClassName = RoslynUtils.GetMemberFQDNWithoutMemberName(node);
-
-		if (_typeToNewFieldDeclarations.TryGetValue(fullClassName, out var newFields))
-		{
-			if (newFields.Contains(fieldName))
-			{
-				var existingLeading = node.GetLeadingTrivia();
-				var existingTrailing = node.GetTrailingTrivia();
-
-				return node
-					.WithLeadingTrivia(existingLeading.Add(SyntaxFactory.Comment("/* ")))
-					.WithTrailingTrivia(existingTrailing.Insert(0, SyntaxFactory.Comment(" */ //Auto-excluded to prevent exceptions - see docs")));
-			}
-		}
-
-		return base.VisitFieldDeclaration(node);
-	}
-}
 }
