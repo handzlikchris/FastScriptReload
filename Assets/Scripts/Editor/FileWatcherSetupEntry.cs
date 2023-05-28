f2fed8a4fe85762b308243fb051499f3310185ae
XuToWei <xw741619720@qq.com>
2023-05-28 16:29:57 +0800

    fix FileWatcherSetupEntry beyond border

--- Assets/Scripts/Editor/FileWatcherSetupEntry.cs
+++ Assets/Scripts/Editor/FileWatcherSetupEntry.cs
@@ -27,7 +27,7 @@ namespace FastScriptReload.Editor
         {
             return new List<IJsonObjectRepresentationRenderingInfo>
             {
-                new JsonObjectRepresentationStringRenderingInfo<FileWatcherSetupEntry>("Path", (e) => e.path, (o, val) => o.path = val, 230),
+                new JsonObjectRepresentationStringRenderingInfo<FileWatcherSetupEntry>("Path", (e) => e.path, (o, val) => o.path = val, 200),
                 new JsonObjectRepresentationStringRenderingInfo<FileWatcherSetupEntry>("Filter", (e) => e.filter, (o, val) => o.filter = val, 100),
                 new JsonObjectRepresentationBoolRenderingInfo<FileWatcherSetupEntry>("Include Subdirectories", (e) => e.includeSubdirectories, (o, val) => o.includeSubdirectories = val, 145),
             };
